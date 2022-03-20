9752e5c3ad586005f6595ffbad222a053741388d
sungkihwan <singahu@naver.com>
2022-03-21 02:32:08 +0900

    first commit

---
+++ billing/src/app.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { OrderCreatedEvent } from './order-created.event';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  handleOrderCreated(orderCreatedEvent: OrderCreatedEvent) {
+    console.log(orderCreatedEvent);
+  }
+}
